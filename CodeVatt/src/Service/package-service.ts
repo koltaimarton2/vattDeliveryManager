1f5a05156984b41622ebb18687908ca87ca8f1b6
Zsigmond Attila <zsigmond.attila.laszlo@students.jedlik.eu>
2026-05-28 10:44:57 +0200

    Done packeges list on the homepage + few fixes

--- CodeVatt/src/Service/package-service.ts
+++ CodeVatt/src/Service/package-service.ts
@@ -3,7 +3,7 @@ import type { Package } from "../Interfaces/package";
 const BASE_URL = "http://localhost:3000/packages"
 
 export async function getPackages(): Promise<Package[]> {
-    let response = fetch(BASE_URL, {method: "GET"})
+    const response = fetch(BASE_URL, {method: "GET"})
     if (!(await response).ok) {
         console.error("HTTP error:", (await response).status)
     }
@@ -14,29 +14,36 @@ export async function getPackage(id: string) : Promise<Package>{
     return await(await fetch(`http://localhost:3000/packages/${id}`)).json()
 }
 
+export async function getStatusPackages(status: 'waiting' | 'pending' | 'done'): Promise<Package[]> {
+    const response = fetch(`${BASE_URL}?status=${status}`, {method: "GET"})
+    if (!(await response).ok) {
+        console.error("HTTP error:", (await response).status)
+    }
+    return (await response).json();
+}
 
 export async function createPackage(pack: Package) : Promise<Package >{
-    let response = fetch(BASE_URL, {
+    const response = fetch(BASE_URL, {
         method: "POST",
-        headers: {"Content-type": "applications/json"},
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify(pack)
     })
     return (await response).json()
 }
 
 export async function updatePackageStatus(id: string, status: 'waiting' | 'pending' | 'done') {
-    let response = fetch(`${BASE_URL}/${id}`, {
+    const response = fetch(`${BASE_URL}/${id}`, {
         method: "PATCH",
-        headers: {"Content-type": "applications/json"},
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify({status: status})
     })
     return (await response).json();
 }
 
 export async function updatePackageProgress(id: string, progress: number) {
-    let response = fetch(`${BASE_URL}/${id}`, {
+    const response = fetch(`${BASE_URL}/${id}`, {
         method: "PATCH",
-        headers: {"Content-type": "applications/json"},
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify({progress: progress})
     })
     return (await response).json();
--- CodeVatt/src/main.ts
+++ CodeVatt/src/main.ts
@@ -1,3 +1,28 @@
-import { simulateDelivery } from "./Service/joint-functions";
+import { simulateDelivery } from "./Service/joint-functions"
+import { getStatusPackages } from "./Service/package-service";
 
-simulateDelivery("c2", "p1")
+
+//simulateDelivery("c2", "p1");
+
+const apphomediv = document.getElementById("app");
+let doneorders = await getStatusPackages('done');
+
+
+function renderMain() {
+    apphomediv!.innerHTML = ``;
+    doneorders.forEach(pack => {
+        const packdiv = document.createElement("div");
+        packdiv.innerHTML = `<div class="card mt-4">
+        <div class="card-header">
+        ${pack.customerName}-${pack.customerZone}
+        
+        </div>
+        <div class="card-body">
+      <h5 class="card-title">${pack.restaurant}</h5>
+      <p class="card-text">${pack.order}</p>
+        </div>
+        </div>`;
+        apphomediv!.appendChild(packdiv);
+    })
+}
+renderMain();
